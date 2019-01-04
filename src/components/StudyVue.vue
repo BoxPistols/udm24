494007076442798064affd8dd8d676a36591b500
A_ito <BoxPistols@users.noreply.github.com>
2019-01-04 20:41:24 +0900

    24

--- src/components/StudyVue.vue
+++ src/components/StudyVue.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="study_wrap">
     <!--
-        6<h1>Vue is Good</h1>
-      <p>{{ user.lastName }} {{ user.firstName }}</p>
+        <h1>Vue is Good</h1>
+        <p>{{ user.lastName }} {{ user.firstName }}</p>
+      <h2 v-show="toggle">Hello</h2>
     -->
-    <h2 v-show="toggle">Hello</h2>
+    <ul>
+      <li v-for="value in user">{{ value }}</li>
+    </ul>
 
     <ul>
-      <li v-for="color in colors">{{ color }}</li>
+      <li v-for="(v, k) in user">{{ k }} : {{ v }}</li>
     </ul>
+
+    <button v-on:click="onClick">Click</button>
+    <p>{{ now }}</p>
+    <!--
+      <ul>
+        <li v-for="color in colors">{{color }}</li>
+      </ul>
+    -->
   </div>
 </template>
 
@@ -17,12 +28,20 @@ export default {
   data() {
     return {
       toggle: true,
-      colors: ["red", "green", "blue"]
-      // user: {
-      //   lastName: "Yamada",
-      //   firstName: "Takashi"
-      // }
+      colors: ["red", "green", "blue"],
+      user: {
+        lastName: "Yamada",
+        firstName: "Taro",
+        age: 28
+      }
     };
+  },
+  methods: {
+    onClick: function() {
+      // this.now = "XXX";
+      this.now = new Date().toLocaleTimeString();
+      alert();
+    }
   }
 };
 </script>
